cc2d4c22e643268488ecbfd197f99b61de2a4af5
Jacob Alberty <jacob.alberty@gmail.com>
2022-06-27 22:37:03 -0500

    Convert to gin

--- controller/http_handler.go
+++ controller/http_handler.go
@@ -4,20 +4,24 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 type HttpHandler struct{}
 
 func (h *HttpHandler) RegisterHandlers() {
-	http.HandleFunc("/inform", h.InformHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := gin.Default()
+
+	router.GET("/inform", h.InformHandler)
+
+	router.Run()
 }
 
-func (h *HttpHandler) InformHandler(w http.ResponseWriter, r *http.Request) {
-	bodyBuffer, _ := ioutil.ReadAll(r.Body)
+func (h *HttpHandler) InformHandler(c *gin.Context) {
+	bodyBuffer, _ := ioutil.ReadAll(c.Request.Body)
 	fn := fmt.Sprintf("tmp/%d", time.Now().Unix())
 	os.WriteFile(fn, bodyBuffer, 0644)
 	log.Printf("Wrote %d bytes to %s\n", len(bodyBuffer), fn)
