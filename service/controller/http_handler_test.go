5dd507a898ded20bc828c9825ea3a2049240c51f
Jacob Alberty <jacob.alberty@gmail.com>
2022-07-01 21:13:04 -0500

    Add test for wifi list

--- service/controller/http_handler_test.go
+++ service/controller/http_handler_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 
 	"github.com/google/jsonapi"
@@ -11,12 +12,113 @@ import (
 	"github.com/jacobalberty/beenfar/service/model"
 )
 
+func TestWifiNetworkList(t *testing.T) {
+	var (
+		err          error
+		req          *http.Request
+		h            *controller.HttpHandler
+		bTmp         bytes.Buffer
+		response     *httptest.ResponseRecorder
+		wifiNetworks []interface{}
+	)
+
+	t.Parallel()
+
+	h = new(controller.HttpHandler)
+	h.RegisterHandlers()
+
+	// Get an empty wifi network list
+	if req, err = http.NewRequest("GET", "/api/wifi", nil); err != nil {
+		t.Fatal(err)
+	}
+
+	response = executeRequest(h, req)
+	if response.Code != http.StatusOK {
+		t.Errorf("Expected status code %v, got %v", http.StatusOK, response.Code)
+	}
+
+	if wifiNetworks, err = jsonapi.UnmarshalManyPayload(response.Body, reflect.TypeOf(new(model.WifiNetworkConfig))); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(wifiNetworks) != 0 {
+		t.Errorf("Expected empty wifi network list, got %v", wifiNetworks)
+
+	}
+
+	// Add a wifi network
+	wifiNetworks = append(wifiNetworks,
+		&model.WifiNetworkConfig{
+			Ssid: "TestWifiNetwork",
+		},
+		&model.WifiNetworkConfig{
+			Ssid: "TestWifiNetwork2",
+		},
+	)
+
+	for _, wifiNetwork := range wifiNetworks {
+		bTmp.Reset()
+
+		err = jsonapi.MarshalPayload(&bTmp, wifiNetwork)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err = http.NewRequest("POST", "/api/wifi", &bTmp)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		response = executeRequest(h, req)
+		if response.Code != http.StatusCreated {
+			t.Errorf("Expected status code %v, got %v", http.StatusCreated, response.Code)
+		}
+	}
+
+	// Get the wifi network list
+	req, err = http.NewRequest("GET", "/api/wifi", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	response = executeRequest(h, req)
+	if response.Code != http.StatusOK {
+		t.Errorf("Expected status code %v, got %v", http.StatusOK, response.Code)
+	}
+
+	if wifiNetworks, err = jsonapi.UnmarshalManyPayload(response.Body, reflect.TypeOf(new(model.WifiNetworkConfig))); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(wifiNetworks) != 2 {
+		t.Errorf("Expected wifi network list with 2 elements, got %v", wifiNetworks)
+	}
+
+	// Delete the wifi network list
+	for _, wifiNetwork := range wifiNetworks {
+		ssid := wifiNetwork.(*model.WifiNetworkConfig).Ssid
+		req, err = http.NewRequest("DELETE", "/api/wifi/"+ssid, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		response = executeRequest(h, req)
+		if response.Code != http.StatusNoContent {
+			t.Errorf("Expected status code %v, got %v", http.StatusNoContent, response.Code)
+		}
+	}
+
+}
+
 func TestWifiNetwork(t *testing.T) {
 	var (
 		h        *controller.HttpHandler
 		bTmp     bytes.Buffer
 		response *httptest.ResponseRecorder
+		testWifi model.WifiNetworkConfig
 	)
+	t.Parallel()
+
 	h = new(controller.HttpHandler)
 	h.RegisterHandlers()
 
@@ -31,7 +133,7 @@ func TestWifiNetwork(t *testing.T) {
 		t.Errorf("Expected status %d, got %d", http.StatusNotFound, response.Code)
 	}
 
-	testWifi := model.WifiNetworkConfig{
+	testWifi = model.WifiNetworkConfig{
 		Ssid:        "test",
 		SecurityKey: "key",
 	}
